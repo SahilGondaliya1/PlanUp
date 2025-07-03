efb55d0eba1811e19e4fff72f166a50f35ade57e
sahil gondaliya <sahilgondaliya789@gmail.com>
2025-07-03 10:47:40 +0530

    src folder creation

--- src/script.js
+++ src/script.js
@@ -1,34 +1,42 @@
-// const { createElement } = require("react");
+
 let taskCount = 0
 
+function createTaskInputWindow(){
+    //creation of input-window-container
+    const container = document.createElement('div');
+    container.setAttribute('class','inputWindow');
+    
+
+    //
+
+    return container;
+}
 
 function inputEvent(){
     //creating inout-window
-    const inputWindow = document.createElement('div');
-    inputWindow.setAttribute('class', 'inputWindow');
+    const inputWindow = createTaskInputWindow();
     //adding window to body;
     document.body.appendChild(inputWindow);
-    //adding input fields
-    const taskTitle = document.createElement('')
 }
 
 function addTask(){
     //creating task div 
-    var task = Object.assign(document.createElement('div'), {id:`task-${++taskCount}`}); task.className = "taskCard";
+    const task = Object.assign(document.createElement('div'), {id:`task-${++taskCount}`});
+    task.className = "taskCard";
 
     //adding to task-grid  
-    let taskGrid = document.querySelector('.taskGrid');
+    const taskGrid = document.querySelector('.taskGrid');
     task.textContent = task.id;
     taskGrid.appendChild(task);
 
     //creating delete button 
-    var deleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
     deleteButton.setAttribute('class','deleteButton');
     deleteButton.textContent = 'delete';
     deleteButton.addEventListener("click", ()=>handleDeleteTask(task.id));
 
     //adding button to taskCard
-    var taskCard = document.getElementById(task.id);
+    const taskCard = document.getElementById(task.id);
     taskCard.appendChild(deleteButton)
 
 }
