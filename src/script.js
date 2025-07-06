53907e163ddaa0872b8f044c6e234103af7e4092
sahil gondaliya <sahilgondaliya789@gmail.com>
2025-07-06 12:43:56 +0530

    key-accesible-feature

--- src/script.js
+++ src/script.js
@@ -1,59 +1,66 @@
 
+document.querySelector(".add-task-button").addEventListener("click", () => {
+  inputEvent();
+});
+document.querySelector(".add-task-button").addEventListener("keydown", (event) => {
+  if(event.key === 'Enter'){
+    inputEvent();
+  }
+});
+
+/**
+ *
+ * @param {String} tasktitle - Task heading displayed on bold
+ * @param {String} description - task information
+ * @param {String} status - pending , completed
+ */
+function createTaskCard(taskTitle) {
+  const template = document.getElementById("taskCardTemplate");
+  //cloneNode(true) is to deep clone the element with child nodes
+  const card = template.content.cloneNode(true);
+  card.querySelector(".task-title").textContent = taskTitle;
+  const taskID = "t" + new Date().getTime();
+  card.setAttribute = ('id',taskID);
+  card.querySelector(".task-delete-button").addEventListener("click", () => handleDeleteTask(taskID));
+
+  return card;
+}
+
+/**
+ * @param {String} taskTitle - task detail to be displayed
+ */
+
+function addTask(taskTitle) {
+  
+  const card = createTaskCard(taskTitle);
+  
+  const taskGrid = document.querySelector(".taskGrid");
+  taskGrid.appendChild(card.querySelector(".task-card"));
+}
+
+/**
+ *
+ * @param {String} taskID - task-id to be deleted
+ */
+function handleDeleteTask(taskID) {
+  var deletingTask = document.getElementById(taskID);
+  console.log(taskID+ `${deletingTask}`);
+  if (deletingTask) {
+    console.log("removing task");
+    deletingTask.remove();
+  }
+}
+
 // let taskID = 0; ---> removing global counter to maintain uniqueness and seperability
 /**
  * @function -creates input winow that shows up on screen
  * @returns {Element} - returns container div
  */
-function createTaskInputWindow() {
+function createInptWinFromTemp() {
+  //get template
+  const template = document.getElementById("inputWindowTemplate");
   //creation of input-window-container
-  const container = document.createElement("div");
-  container.setAttribute("class", "inputWindow");
-
-  //creation of task-inputs
-  const lable = document.createElement("label");
-  lable.textContent = "Task:";
-  lable.htmlFor = "input-task-title";
-  lable.className = "input-window-item";
-
-  const taskInput = document.createElement("input");
-  taskInput.id = "input-task-title";
-  taskInput.type = "text";
-  taskInput.className = "input-window-item";
-
-  //creation of container div to pack the buttons
-  const buttonDiv = document.createElement("div");
-  buttonDiv.id = "buttonDiv";
-  buttonDiv.className = "input-window-item";
-  //creation of buttons
-  const submitBtn = document.createElement("button");
-  submitBtn.id = "submitBtn";
-  submitBtn.textContent = "Add";
-  submitBtn.className = "input-window-button";
-
-  const cancleBtn = document.createElement("button");
-  cancleBtn.id = "cancleBtn";
-  cancleBtn.textContent = "Cancle";
-  cancleBtn.className = "input-window-button";
-
-  //bind events => meaning adding event Listeners
-  submitBtn.onclick = () => {
-    let taskTitle = document.getElementById('input-task-title').value;
-    document.body.querySelector(".inputWindow").remove();
-    if(taskTitle){
-      addTask(taskTitle);
-    }
-  };
-
-  cancleBtn.onclick = () => {
-    document.body.querySelector(".inputWindow").remove();
-  };
-
-  container.appendChild(lable);
-  container.appendChild(taskInput);
-  buttonDiv.appendChild(submitBtn);
-  buttonDiv.appendChild(cancleBtn);
-  container.appendChild(buttonDiv);
-
+  const container = template.content.cloneNode(true).firstElementChild;
   return container;
 }
 
@@ -63,56 +70,31 @@ function createTaskInputWindow() {
 
 function inputEvent() {
   //creating inout-window
-  const inputWindow = createTaskInputWindow();
+  const inputWindow  = createInptWinFromTemp();
   //adding window to body;
   document.body.appendChild(inputWindow);
+  //add event Listeners
+  document
+    .querySelector("#input-submit-button").addEventListener("click", () => {
+      let taskTitle = document.getElementById("input-task-title").value;
+      document.body.querySelector(".inputWindow").remove();
+      if (taskTitle) {
+        addTask(taskTitle);
+      }
+    });
+
+  inputWindow.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      let taskTitle = document.getElementById("input-task-title").value;
+      document.body.querySelector(".inputWindow").remove();
+      if (taskTitle) {
+        addTask(taskTitle);
+      }
+    }
+  });
+
+  document.querySelector("#input-cancle-button").onclick = () => {
+    document.body.querySelector(".inputWindow").remove();
+  };
 }
 
-document.querySelector('.add-task-button').addEventListener("click", () =>{
-  inputEvent();
-});
-
-
-/**
- * 
- * @param {String} tasktitle - Task heading displayed on bold
- * @param {String} description - task information
- * @param {String} status - pending , completed
- */
-function createTaskCard(taskTitle){
-  const template  = document.getElementById("taskCardTemplate");
-   //cloneNode(true) is to deep clone the element with child nodes 
-  const card = template.content.cloneNode(true);
-  card.querySelector('.task-title').textContent = taskTitle;
-  card.querySelector('.delete-button').addEventListener("click", ()=>handleDeleteTask(taskId));
-  return card;
-}
-
-/**
- * @param {String} taskTitle - task detail to be displayed
- */
-
-function addTask(taskTitle) {
-
-  const taskID = 't'+ new Date().getTime();
-  const card = createTaskCard(taskTitle);
-  card.id = taskID;
-  if(card){console.log(card)}
-  const taskGrid = document.querySelector(".taskGrid");
-  taskGrid.appendChild(card);
-}
-
-
-
-
-function handleDeleteTask(taskId) {
-  var deletingTask = document.getElementById(taskId);
-  if (deletingTask) {
-    console.log("removing task");
-    deletingTask.remove();
-  }
-}
-
-function changColor() {
-  var a = document.getElementsByClassName("add-task-wrapper");
-}
